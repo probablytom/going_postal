39f3a90f273553eaa008b836212530e98885c371
Tom Wallis <github@tomwallis.net>
2018-01-15 16:40:54 +0000

    Moved into an actual package

---
+++ postal/__init__.py
@@ -0,0 +1,6 @@
+# Anything that gets passed on from theatre_ag here
+from theatre_ag import TaskQueueActor, Cast, Episode, Idling, default_cost
+from theatre_ag import SynchronizingClock, format_task_trees
+
+# Overridden / new stuff here
+from .postal import Actor
